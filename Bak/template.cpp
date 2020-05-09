5ad3436a977471fbbcdd72eb4bdd2c25b8054e7f
Anh Nguyen <62135127+jonasngyn@users.noreply.github.com>
2020-05-09 15:58:24 +0700

    Create template.cpp

---
+++ Bak/template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define JN Jonasngyn
+#define pb push_back
+#define FOR(i, a, b) for(int i = int(a); i <= int(b); i++)
+#define FORD(i, a, b) for(int i = int(a); i >= int(b); i--)
+#define REP(i, r) for(int i = 0; i < r; i++)
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define READFILE freopen("input.txt", "r", stdin)
+#define WRITEFILE freopen("output.txt", "w", stdout);
+#define ll long long
+#define ii pair<int, int>
+#define fi first
+#define se second
+using namespace std;
+
+const int MAX=-1;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        READFILE;
+    #endif
+    FAST;
+
+    return 0;
+}
